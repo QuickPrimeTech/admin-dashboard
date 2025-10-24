185896734b9de6f529e21e5f68ee3d857103ee7f
QuickPrimeTech <quickprimetech@gmail.com>
2025-10-24 15:49:33 +0300

    changed the queryClient to have infinite stale time thus making the cached data always fresh everytime

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -4,7 +4,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    },
+  },
+});
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
