3257d23b036a824b373903ad33882b6bbefd2119
Meshack-kipkemoi <meshackkipkemoi3@gmail.com>
2025-10-27 21:41:51 +0300

    made the categories in both of the basic info forms to be dynamic

--- src/sections/menu/add/basic-info-section.tsx
+++ src/sections/menu/add/basic-info-section.tsx
@@ -18,13 +18,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { BasicInfoFormData, basicInfoSchema } from "@/schemas/menu";
--- src/sections/menu/edit/basic-info-section.tsx
+++ src/sections/menu/edit/basic-info-section.tsx
@@ -19,13 +19,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { BasicInfoFormData, basicInfoSchema } from "@/schemas/menu";
 import {
   InputGroup,
