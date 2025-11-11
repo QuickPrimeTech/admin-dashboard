b78ef7dd7299fd275812dc05e62139c61297457e
QuickPrimeTech <quickprimetech@gmail.com>
2025-11-11 13:37:50 +0300

    added icons to the theme dropdown

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Sun, Moon, Laptop, LucideProps } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@ui/button";
@@ -12,29 +12,39 @@ import {
   DropdownMenuTrigger,
 } from "@ui/dropdown-menu";
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
+type ThemeType = "light" | "dark" | "system";
 
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const ThemeIcon: Record<ThemeType,  React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>> = {
+    light: Sun,
+    dark: Moon,
+    system: Laptop,
+  };
+  // Ensure theme is one of ThemeType
+  const currentTheme = (theme || "system") as ThemeType;
+  const CurrentIcon = ThemeIcon[currentTheme];
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          {<CurrentIcon />}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          <Sun className="mr-2 h-4 w-4" /> Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          <Moon className="mr-2 h-4 w-4" /> Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          <Laptop className="mr-2 h-4 w-4" /> System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
 }
+
