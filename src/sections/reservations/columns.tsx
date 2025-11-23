60df9109075cb00ec72773e554ca35c397b8f4b5
QuickPrimeTech <quickprimetech@gmail.com>
2025-11-23 23:33:52 +0300

    finished a very basic table with basic filtering for the reservations page

--- src/sections/reservations/columns.tsx
+++ src/sections/reservations/columns.tsx
@@ -1,7 +1,9 @@
 "use client";
-
+import { Button } from "@/components/ui/button";
 import { Reservation } from "@/types/reservations";
 import { ColumnDef } from "@tanstack/react-table";
+import { Calendar } from "lucide-react";
+import Link from "next/link";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -14,12 +16,23 @@ export type Payment = {
 
 export const columns: ColumnDef<Reservation>[] = [
   {
+    id: "index",
+    header: "No",
+    cell: ({ row }) => <span>{row.index + 1}</span>, // simple numbering starting from 1
+  },
+  {
     accessorKey: "name",
     header: "Name",
   },
   {
     accessorKey: "date",
     header: "Date booked",
+    cell: ({ row }) => {
+      const raw = row.getValue("date") as string; // "2025-11-20"
+      const date = new Date(raw);
+
+      return date.toLocaleDateString("en-GB"); // → "20/11/2025"
+    },
   },
   {
     accessorKey: "time",
@@ -28,6 +41,9 @@ export const columns: ColumnDef<Reservation>[] = [
   {
     accessorKey: "guests",
     header: "Guest No",
+    cell: ({ row }) => {
+      return <p className="text-center">{row.getValue("guests")}</p>;
+    },
   },
   {
     accessorKey: "phone",
@@ -36,5 +52,20 @@ export const columns: ColumnDef<Reservation>[] = [
   {
     accessorKey: "email",
     header: "Email",
+  },
+  {
+    id: "actions", // must have a unique id if no accessorKey
+    header: "Actions",
+    cell: ({ row }) => {
+      const reservationId = row.original;
+      return (
+        <Button variant={"outline"} asChild size={"xs"}>
+          <Link href={`/reservations/${reservationId.id}`}>
+            <Calendar />
+            See Details
+          </Link>
+        </Button>
+      );
+    },
   },
 ];
--- src/sections/reservations/data-table.tsx
+++ src/sections/reservations/data-table.tsx
@@ -1,12 +1,12 @@
 "use client";
-
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-
 import {
   Table,
   TableBody,
@@ -15,6 +15,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Search } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -25,56 +33,107 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [globalFilter, setGlobalFilter] = useState([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    globalFilterFn: "includesString",
+    state: {
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter,
   });
 
   return (
-    <div className="overflow-hidden rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
+    <div>
+      <div className="flex items-center py-4">
+        <InputGroup className="max-w-sm">
+          <InputGroupInput
+            placeholder="Search any data..."
+            onChange={(e) => table.setGlobalFilter(String(e.target.value))}
+          />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+          {globalFilter.length > 0 && (
+            <InputGroupAddon align={"inline-end"}>
+              {table.getRowModel().rows.length} results
+            </InputGroupAddon>
           )}
-        </TableBody>
-      </Table>
+        </InputGroup>
+      </div>
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
